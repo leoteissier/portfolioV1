31e0cfef99d8db7eeb21d1887690b81fffb6797e
Léo Teissier <leo.teissier@numericable.fr>
2023-02-17 10:45:24 +0100

    commit

--- src/components/projects/Css3.vue
+++ src/components/projects/Css3.vue
@@ -5,10 +5,10 @@
         <div class="img-card">
           <img src="" alt="">
         </div>
-        <img class="logo-card-css" src="../../assets/css3-black.svg" alt="logo javascript">
+        <img class="logo-card-css" src="../../assets/css3-black.svg" alt="logo css">
         <div class="description-card border-top">
           <p>Here is a solar system made in very dry css that I made when I started programming.</p>
-          <a class="button" href="./../travaux/systeme-solaire/index.html" target="_blank">view-project</a>
+          <a class="button" href="" target="_blank">view-project</a>
         </div>
       </div>
     </div>
