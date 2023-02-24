610ef9419230ee1e3a3cb21601976ffcf0fa5e3c
Léo Teissier <leo.teissier@numericable.fr>
2023-02-24 14:34:15 +0100

    update portfolio

--- src/components/projects/Css3.vue
+++ src/components/projects/Css3.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="project">
-      <p class="margin-bottom-10px"><span class="color-secondary">Project Css </span>// _....</p>
+      <p class="margin-bottom-10px"><span class="color-secondary">Project Css </span>// _Flip Coin</p>
       <div class="card">
         <div class="img-card">
           <img src="" alt="">
         </div>
         <img class="logo-card-css" src="/assets/css3-black.svg" alt="logo css">
         <div class="description-card border-top">
-          <p>Duis aute irure dolor in velit esse cillum dolore.</p>
-          <a class="button" target="_blank">view-project</a>
+          <p>
+            My flipcoin app is a fun and interactive way to make decisions.
+            With just a flip of a virtual coin, users can quickly and easily settle any dispute or make a random choice.
+          </p>
+          <a class="button" href="https://flipcoin.leoteissier.fr" target="_blank">view-project</a>
         </div>
       </div>
     </div>
