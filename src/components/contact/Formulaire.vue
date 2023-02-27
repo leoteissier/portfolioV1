71bdd3e76eb4db6d2dbb751f1c7f3d5800d18b06
leote <leo.teissier@etudiant.univ-reims.fr>
2023-02-27 20:01:43 +0100

    modif formualaire

--- src/components/contact/Formulaire.vue
+++ src/components/contact/Formulaire.vue
@@ -1,33 +1,35 @@
 <template>
   <div class="formulaire">
     <!--form submitted-->
-    <div v-if="formSubmitted" class="width-75">
-      <div class="flex-column flex-center">
-        <h2 class="color-white">Thank you!</h2>
-        <p>Your message has been accepted. You will recieve answer really soon!</p>
-        <button @click="formSubmitted = false">send-new-message</button>
-      </div>
-    </div>
+<!--    <div class="width-75">-->
+<!--      <div class="flex-column flex-center">-->
+<!--        <h2 class="color-white">Thank you!</h2>-->
+<!--        <p>Your message has been accepted. You will recieve answer really soon!</p>-->
+<!--        <button>send-new-message</button>-->
+<!--      </div>-->
+<!--    </div>-->
     <!--end form submitted-->
     <!--form-->
-    <form ref="form" @submit.prevent="submitForm" v-else>
+    <form @submit.prevent>
       <div class="flex-column margin-top-10px">
         <label class="margin-bottom-10px" for="name">_name :</label>
         <input type="text" v-model.trim="name" id="name" name="name" placeholder="votre prénom"/>
       </div>
       <div class="flex-column margin-top-10px">
         <label class="margin-bottom-10px" for="email">_email :</label>
-        <input type="email" v-model.trim="email" id="email" name="email" placeholder="Votre email"/>
+        <input type="email" v-model.trim="email" id="email" name="email" placeholder="Votre email">
+        <div v-if="emailIsValid" class="error-message">Adresse e-mail invalide</div>
       </div>
       <div class="flex-column margin-top-10px">
         <label class="margin-bottom-10px" for="email">_subject :</label>
-        <input type="text" v-model.trim="subject" id="subject" name="subject" placeholder="Votre sujet"/>&
+        <input type="text" v-model.trim="subject" id="subject" name="subject" placeholder="Votre sujet"/>
       </div>
       <div class="flex-column margin-top-10px">
         <label class="margin-bottom-10px" for="message">_message :</label>
         <textarea name="message" v-model.trim="message" id="message" cols="30" rows="10" placeholder="Votre message"></textarea>
       </div>
-      <button type="submit">_submit_message</button>
+      <button @click="submitForm">_submit_message</button>
+      <div v-if="showErrorMessage" class="error-message">Please fill in all fields</div>
     </form>
     <!--end form-->
   </div>
@@ -35,42 +37,44 @@
     <div class="margin-left-50px width-form-result">
       <div>
         <div class="flex">
-          <p class="margin-right-20px">1</p><p><span class="color-quinary">const</span><span class="color-secondary"> button</span><span class="color-quinary"> =</span><span class="color-secondary"> document.querySelector</span>(<span class="color-tertiary">'#sendBtn'</span>);</p>
+          <p class="margin-right-20px">1</p><p class="color-white"><span class="color-quinary">const</span><span class="color-secondary"> button</span><span class="color-quinary"> =</span><span class="color-secondary"> document.querySelector</span>(<span class="color-tertiary">'#sendBtn'</span>);</p>
         </div>
         <div>
           <p class="hidden">1</p>
         </div>
         <div class="flex">
-          <p class="margin-right-20px">2</p><p><span class="color-quinary">const</span><span class="color-secondary"> message</span><span class="color-quinary"> =</span> {</p>
+          <p class="margin-right-20px">2</p><p class="color-white"><span class="color-quinary">const</span><span class="color-secondary"> message</span><span class="color-quinary"> =</span> {</p>
         </div>
         <div class="flex">
-          <p class="margin-right-20px">3</p><p class="margin-left-10px"><span class="color-secondary">name</span>: <span class="color-tertiary">"{{ name }}"</span>,</p>
+          <p class="margin-right-20px">3</p><p class="margin-left-10px color-white"><span class="color-secondary">name</span>: <span class="color-tertiary">"{{ name }}"</span>,</p>
         </div>
         <div class="flex">
-          <p class="margin-right-20px">4</p><p class="margin-left-10px"><span class="color-secondary">email</span>: <span class="color-tertiary">"{{ email }}"</span>,</p>
+          <p class="margin-right-20px">4</p><p class="margin-left-10px color-white"><span class="color-secondary">email</span>: <span class="color-tertiary">"{{ email }}"</span>,</p>
         </div>
         <div class="flex">
-          <p class="margin-right-20px">5</p><p class="margin-left-10px"><span class="color-secondary">subject</span>: <span class="color-tertiary">"{{ subject }}"</span>,</p>
+          <p class="margin-right-20px">5</p><p class="margin-left-10px color-white"><span class="color-secondary">subject</span>: <span class="color-tertiary">"{{ subject }}"</span>,</p>
         </div>
         <div class="flex">
-          <p class="margin-right-20px">6</p><p class="margin-left-10px width-100"><span class="color-secondary">message</span>: <span class="color-tertiary" style="overflow-wrap: break-word;">"{{ message }}"</span>,</p>
+          <p class="margin-right-20px">6</p><p class="margin-left-10px color-white width-100"><span class="color-secondary">message</span>: <span class="color-tertiary">"{{ message }}"</span>,</p>
         </div>
         <div class="flex">
-          <p class="margin-right-20px">7</p><p class="color-secondary">date<span class="color-font">:</span> <span class="color-tertiary">"{{ date }}"</span></p>
+          <p class="margin-right-20px">7</p><p class="margin-left-10px color-white"><span class="color-secondary">date</span>: <span class="color-tertiary">"{{ date }}"</span></p>
         </div>
         <div class="flex">
-          <p class="margin-right-20px">8</p><p>}</p>
+          <p class="margin-right-20px">8</p><p class="color-white">}</p>
         </div>
         <div>
           <p class="margin-right-20px">9</p>
         </div>
         <div class="flex">
-          <p class="margin-right-20px">10</p><p><span class="color-secondary">button</span>.<span class="color-secondary">addEventListener</span>(<span class="color-tertiary">'click'</span>, () <span class="color-quinary">=></span> {</p>
+          <p class="margin-right-20px">10</p><p class="color-white"><span class="color-secondary">button</span>.<span class="color-secondary">addEventListener</span>(<span class="color-tertiary">'click'</span>, () <span class="color-quinary">=></span> {</p>
         </div>
         <div class="flex">
-          <p class="margin-right-20px">11</p><p><span class="color-secondary">form</span>.<span class="color-secondary">send</span>(<span class="color-secondary">message</span>);</p>
+          <p class="margin-right-20px">11</p><p class="margin-left-10px color-white"><span class="color-secondary">form</span>.<span class="color-secondary">send</span>(<span class="color-secondary">message</span>);</p>
         </div>
-        <p>})</p>
+        <div class="flex">
+          <p class="margin-right-20px">12</p><p class="color-white">});</p>
+        </div>
       </div>
     </div>
     <div class="scroll border-left">
@@ -88,18 +92,52 @@ export default {
       email: "",
       subject: "",
       message: "",
-      date: new Date().toLocaleDateString(),
-      formSubmitted: false,
+      emailIsValid: false,
+      showErrorMessage: false,
+      date: new Date().toLocaleString(),
     };
+  },
+  methods: {
+    submitForm() {
+      // Vérifiez si les champs requis sont remplis
+      if (!this.name || !this.email || !this.subject || !this.message) {
+        this.showErrorMessage = true;
+        return;
+      } else{
+        this.showErrorMessage = false;
+      }
+
+      // Vérifiez si l'email est valide
+      if (!this.validateEmail(this.email)) {
+        this.emailIsValid = true;
+        return;
+      } else{
+        this.emailIsValid = false;
+      }
+
+      // Créez un objet de message
+      const message = {
+        name: this.name,
+        email: this.email,
+        subject: this.subject,
+        message: this.message,
+        date: new Date().toUTCString()
+      };
+
+      // Envoyez le message (remplacez cela par votre propre code d'envoi de formulaire)
+      console.log(message);
+    },
+    validateEmail(email) {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(email);
+    }
+
   },
   watch: {
     name() {
       localStorage.name = this.name;
     },
-    email(value) {
-      // binding this to the data value in the email input
-      this.email = value;
-      this.validateEmail(value);
+    email() {
       localStorage.email = this.email;
     },
     subject() {
@@ -107,17 +145,6 @@ export default {
     },
     message() {
       localStorage.message = this.message;
-    },
-  },
-  methods: {
-    submitForm() {
-      // Simuler l'envoi du formulaire avec succès (en vrai on enverrait les données au serveur)
-      this.formSubmitted = true;
-      // Réinitialiser les champs du formulaire
-      this.name = '';
-      this.email = '';
-      this.subject = '';
-      this.message = '';
     },
   },
 };
