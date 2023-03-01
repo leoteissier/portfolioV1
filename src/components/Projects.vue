681c5af276d1fb9f764ae635c6511afebe767a38
leote <leo.teissier@etudiant.univ-reims.fr>
2023-03-01 14:54:38 +0100

    ajout img + amélioration du filtre sur Projects.vue

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,12 +4,12 @@ export default {
   data() {
     return {
       langages: [
-          'Javascript',
-          'Css',
-          'Vuejs',
-          'React',
-          'Threejs',
-          'Symfony',
+        'Javascript',
+        'Css',
+        'Vuejs',
+        'React',
+        'Threejs',
+        'Symfony',
       ],
       projets: [
         {
@@ -17,7 +17,7 @@ export default {
           titre: 'Weather App',
           langages: 'Javascript',
           description: 'My weather app is a simple and user-friendly application that provides real-time weather updates for any location.',
-          // image: '',
+          image: 'weather-app.png',
           lien: 'https://weatherapp.leoteissier.fr',
         },
         {
@@ -25,7 +25,7 @@ export default {
           titre: 'Flip Coin',
           langages: 'Css',
           description: 'My flipcoin app is a fun and interactive way to make decisions. With just a flip of a virtual coin, users can quickly and easily settle any dispute or make a random choice.',
-          // image: '',
+          image: 'flip-coin.png',
           lien: 'https://flipcoin.leoteissier.fr',
         },
         {
@@ -33,7 +33,7 @@ export default {
           titre: 'Todo List',
           langages: 'Vuejs',
           description: 'Une description de mon projet 3.',
-          // image: '',
+          image: '',
           lien: 'https://todolist.leoteissier.fr',
         },
         {
@@ -41,7 +41,7 @@ export default {
           titre: 'Todo List',
           langages: 'React',
           description: 'Une description de mon projet 4.',
-          // image: '',
+          image: '',
           lien: 'https://todolist.leoteissier.fr',
         },
         {
@@ -49,7 +49,7 @@ export default {
           titre: 'Todo List',
           langages: 'Threejs',
           description: 'Une description de mon projet 5.',
-          // image: '',
+          image: '',
           lien: 'https://todolist.leoteissier.fr',
         },
         {
@@ -57,7 +57,7 @@ export default {
           titre: 'Todo List',
           langages: 'Symfony',
           description: 'Une description de mon projet 6.',
-          // image: '',
+          image: '',
           lien: 'https://todolist.leoteissier.fr',
         },
       ],
@@ -79,51 +79,51 @@ export default {
 </script>
 
 <template>
-    <div id="projects">
-      <div class="nom-page1 border-bottom"><img class="petit-logo-arrow" src="/assets/arrow-drop-down.svg" alt=""><p>projects</p></div>
-      <div class="nom-page2 border-left border-bottom border-right"><p>projects</p><img class="petit-logo-cross" src="/assets/close.svg" alt=""></div>
-      <div class="nom-page3 border-bottom"></div>
-      <div class="details-projects flex">
+  <div id="projects">
+    <div class="nom-page1 border-bottom"><img class="petit-logo-arrow" src="/assets/arrow-drop-down.svg" alt=""><p>projects</p></div>
+    <div class="nom-page2 border-left border-bottom border-right"><p>projects</p><img class="petit-logo-cross" src="/assets/close.svg" alt=""></div>
+    <div class="nom-page3 border-bottom"></div>
+    <div class="details-projects flex">
 
-        <div class="flex-center" v-for="l in langages" :key="l">
-          <div class="position-checkbox">
-            <label class="checkbox">
-              <input type="checkbox" v-model="checkedLangages" :value="l" />
-              <span class="checkmark"></span>
-            </label>
-          </div>
-          <img class="logo-projects" :src="`/assets/${l}.svg`" :alt="`${l} logo`">
-          <p>{{ l }}</p>
+      <div class="flex-center" v-for="l in langages" :key="l">
+        <div class="position-checkbox">
+          <label class="checkbox">
+            <input type="checkbox" v-model="checkedLangages" :value="l" />
+            <span class="checkmark"></span>
+          </label>
         </div>
-
+        <img class="logo-projects" :src="`/assets/${l}.svg`" :alt="`${l} logo`">
+        <p>{{ l }}</p>
       </div>
 
-      <div class="border-left" id="list-projects">
+    </div>
 
-        <section class="list-projects">
-          <div v-for="p in filteredProjets" :key="p.id">
-            <div class="project">
-              <p class="margin-bottom-10px"><span class="color-secondary">Project {{ p.langages }}</span> // _{{ p.titre }}</p>
-              <div class="card">
-                <div class="img-card">
-                  <img :class="`logo-card-${p.langages}`" :src="`/assets/${p.langages}-black.svg`" :alt="`${p.titre} logo`" />
-                </div>
-                <div class="description-card border-top">
-                  <p>{{ p.description }}</p>
-                  <a class="button" :href="p.lien" target="_blank">view-project</a>
-                </div>
+    <div class="border-left" id="list-projects">
+
+      <section class="list-projects">
+        <div v-for="p in filteredProjets" :key="p.id">
+          <div class="project">
+            <p class="margin-bottom-10px"><span class="color-secondary">Project {{ p.langages }}</span> // _{{ p.titre }}</p>
+            <div class="card">
+              <div class="img-card">
+                <img class="img-projet" :src="`/assets/${p.image}`">
+                <img :class="`logo-card-${p.langages}`" :src="`/assets/${p.langages}-black.svg`" :alt="`${p.titre} logo`" />
+              </div>
+              <div class="description-card border-top">
+                <p>{{ p.description }}</p>
+                <a class="button" :href="p.lien" target="_blank">view-project</a>
               </div>
             </div>
           </div>
-        </section>
-        <div class="scroll border-left">
-          <span></span>
         </div>
-
+      </section>
+      <div class="scroll border-left">
+        <span></span>
       </div>
+
+    </div>
   </div>
 </template>
 
 <style scoped>
-
 </style>
