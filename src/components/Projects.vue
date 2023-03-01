b1764673c391296304c6536c9dfced2070c89233
Léo Teissier <100595766+leoteissier@users.noreply.github.com>
2023-03-01 14:51:25 +0100

    Update Projects.vue

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -15,50 +15,18 @@ export default {
         {
           id: 1,
           titre: 'Weather App',
-          langages: 'Javascript',
+          categories: ['Javascript'],
           description: 'My weather app is a simple and user-friendly application that provides real-time weather updates for any location.',
-          // image: '',
+          image: 'weather-app.png',
           lien: 'https://weatherapp.leoteissier.fr',
         },
         {
           id: 2,
           titre: 'Flip Coin',
-          langages: 'Css',
+          categories: ['Css', 'Javascript'],
           description: 'My flipcoin app is a fun and interactive way to make decisions. With just a flip of a virtual coin, users can quickly and easily settle any dispute or make a random choice.',
-          // image: '',
+          image: 'flip-coin.png',
           lien: 'https://flipcoin.leoteissier.fr',
-        },
-        {
-          id: 3,
-          titre: 'Todo List',
-          langages: 'Vuejs',
-          description: 'Une description de mon projet 3.',
-          // image: '',
-          lien: 'https://todolist.leoteissier.fr',
-        },
-        {
-          id: 4,
-          titre: 'Todo List',
-          langages: 'React',
-          description: 'Une description de mon projet 4.',
-          // image: '',
-          lien: 'https://todolist.leoteissier.fr',
-        },
-        {
-          id: 5,
-          titre: 'Todo List',
-          langages: 'Threejs',
-          description: 'Une description de mon projet 5.',
-          // image: '',
-          lien: 'https://todolist.leoteissier.fr',
-        },
-        {
-          id: 6,
-          titre: 'Todo List',
-          langages: 'Symfony',
-          description: 'Une description de mon projet 6.',
-          // image: '',
-          lien: 'https://todolist.leoteissier.fr',
         },
       ],
       checkedLangages: [],
@@ -70,7 +38,7 @@ export default {
         return this.projets;
       } else {
         return this.projets.filter((p) =>
-            this.checkedLangages.includes(p.langages)
+            this.checkedLangages.some(c => p.categories.includes(c))
         );
       }
     },
@@ -79,48 +47,59 @@ export default {
 </script>
 
 <template>
-    <div id="projects">
-      <div class="nom-page1 border-bottom"><img class="petit-logo-arrow" src="/assets/arrow-drop-down.svg" alt=""><p>projects</p></div>
-      <div class="nom-page2 border-left border-bottom border-right"><p>projects</p><img class="petit-logo-cross" src="/assets/close.svg" alt=""></div>
-      <div class="nom-page3 border-bottom"></div>
-      <div class="details-projects flex">
-
-        <div class="flex-center" v-for="l in langages" :key="l">
-          <div class="position-checkbox">
-            <label class="checkbox">
-              <input type="checkbox" v-model="checkedLangages" :value="l" />
-              <span class="checkmark"></span>
-            </label>
-          </div>
-          <img class="logo-projects" :src="`/assets/${l}.svg`" :alt="`${l} logo`">
-          <p>{{ l }}</p>
+  <div id="projects">
+    <div class="nom-page1 border-bottom">
+      <img class="petit-logo-arrow" src="/assets/arrow-drop-down.svg" alt="" />
+      <p>projects</p>
+    </div>
+    <div class="nom-page2 border-left border-bottom border-right">
+      <p>projects</p>
+      <img class="petit-logo-cross" src="/assets/close.svg" alt="" />
+    </div>
+    <div class="nom-page3 border-bottom"></div>
+    <div class="details-projects flex">
+      <div class="flex-center" v-for="l in langages" :key="l">
+        <div class="position-checkbox">
+          <label class="checkbox">
+            <input type="checkbox" v-model="checkedLangages" :value="l" />
+            <span class="checkmark"></span>
+          </label>
         </div>
-
+        <img class="logo-projects" :src="`/assets/${l}.svg`" :alt="`${l} logo`" />
+        <p>{{ l }}</p>
       </div>
-
-      <div class="border-left" id="list-projects">
-
-        <section class="list-projects">
-          <div v-for="p in filteredProjets" :key="p.id">
-            <div class="project">
-              <p class="margin-bottom-10px"><span class="color-secondary">Project {{ p.langages }}</span> // _{{ p.titre }}</p>
-              <div class="card">
-                <div class="img-card">
-                  <img :class="`logo-card-${p.langages}`" :src="`/assets/${p.langages}-black.svg`" :alt="`${p.titre} logo`" />
-                </div>
-                <div class="description-card border-top">
-                  <p>{{ p.description }}</p>
-                  <a class="button" :href="p.lien" target="_blank">view-project</a>
-                </div>
+    </div>
+    <div class="border-left" id="list-projects">
+      <section class="list-projects">
+        <div v-for="p in filteredProjets" :key="p.id">
+          <div v-if="checkedLangages.some(c => p.categories.includes(c))" class="project">
+            <div class="project-header">
+              <p class="margin-bottom-10px">
+                <span class="color-secondary">Project {{ p.categories.join(', ') }}</span> // _{{ p.titre }}
+              </p>
+            </div>
+            <div class="card">
+              <div class="img-card">
+                <img class="img-projet" :src="`/assets/${p.image}`">
+                <img v-if="p.categories.includes('Javascript')" class="logo-card-Javascript" :src="`/assets/Javascript-black.svg`" :alt="`Javascript logo`" />
+                <img v-if="p.categories.includes('Css')" class="logo-card-Css" :src="`/assets/Css-black.svg`" :alt="`Css logo`" />
+                <img v-if="p.categories.includes('Vuejs')" class="logo-card-Vuejs" :src="`/assets/Vuejs-black.svg`" :alt="`Vuejs logo`" />
+                <img v-if="p.categories.includes('React')" class="logo-card-React" :src="`/assets/React-black.svg`" :alt="`React logo`" />
+                <img v-if="p.categories.includes('Threejs')" class="logo-card-Threejs" :src="`/assets/Threejs-black.svg`" :alt="`Threejs logo`" />
+                <img v-if="p.categories.includes('Symfony')" class="logo-card-Symfony" :src="`/assets/Symfony-black.svg`" :alt="`Symfony logo`" />
               </div>
             </div>
+            <div class="description-card">
+              <p>{{ p.description }}</p>
+              <a class="button" :href="p.lien" target="_blank">view-project</a>
+            </div>
           </div>
-        </section>
-        <div class="scroll border-left">
-          <span></span>
         </div>
-
+      </section>
+      <div class="scroll border-left">
+        <span></span>
       </div>
+    </div>
   </div>
 </template>
 
